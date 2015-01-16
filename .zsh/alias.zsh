fcb9dcae55f9b3292f4cc35d98446d34d9bdf9f2
namusyaka <namusyaka@gmail.com>
2015-01-16 15:33:44 +0900

    Add gls as an alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -5,6 +5,7 @@ alias sed='gsed'
 
 alias commit-log='git shortlog --summary -n'
 alias blame='git log -p -S'
+alias gls='git ls-files .'
 
 # Don't forget to use `bundle install --binstubs`
 alias be='bundle exec'
