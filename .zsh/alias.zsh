e77db984cd6997cb94559c6a16c548acc56a1904
namusyaka <namusyaka@gmail.com>
2015-03-11 01:16:34 +0900

    evil

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -6,6 +6,7 @@ alias sed='gsed'
 alias commit-log='git shortlog --summary -n'
 alias blame='git log -p -S'
 alias gls='git ls-files .'
+alias smplsed='find . -type f -print0 | xargs -0 sed -i -e'
 
 # Don't forget to use `bundle install --binstubs`
 alias be='bundle exec'
